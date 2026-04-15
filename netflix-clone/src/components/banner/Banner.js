d477ba51f5a51c3e1c9a7dc571e922b41b2b8f65
Ertiban <ertibandbb@gmail.com>
2026-04-15 23:43:36 +0300

    Update Banner.js

---
+++ netflix-clone/src/components/banner/Banner.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+import React from "react";
+
+const Banner = () => {
+    const[movie, setmovie] = useState([]);
+    useEffect(() => {
+  (async () => {
+    try {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length)
+        ]
+      );
+    } catch (error) {
+      console.log("error", error);
+    }
+  })();
+}, []);
+  return (
+    <div
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+
+        <div className="banner__buttons">
+          <button className="banner__button play">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h1>
+      </div>
+
+      <div className="banner--fadeBottom" />
+    </div>
+  );
+};
+
+export default Banner;
