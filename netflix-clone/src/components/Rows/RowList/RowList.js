923cb858e35c7c8e7b75986d71a3e9412f5af7a4
Ertiban <ertibandbb@gmail.com>
2026-04-17 01:55:19 +0300

    Update RowList.js

--- netflix-clone/src/components/Rows/RowList/RowList.js
+++ netflix-clone/src/components/Rows/RowList/RowList.js
@@ -14,24 +14,22 @@ const RowList = () => {
        isLargeRow={true}
   /> 
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated}
-       isLargeRow={true}
+       
   /> 
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}
-       isLargeRow={true}
+       
       />
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}
-       isLargeRow={true}
+       
       />
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}
-       isLargeRow={true}
+       
       />
       <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}
-       isLargeRow={true}
+       
       />
       <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}
-       isLargeRow={true}
-      />
-
+       />
     </>
   )
 }
