5b648c92a2bd7ca5ac527f7cc02ce022be73f149
Ertiban <ertibandbb@gmail.com>
2026-04-16 03:57:36 +0300

    Update Row.js

--- netflix-clone/src/components/Rows/Row/Row.js
+++ netflix-clone/src/components/Rows/Row/Row.js
@@ -1,10 +1,70 @@
-import React from 'react'
-import "./Row.css"
+import React, { useEffect, useState } from "react";
+import "./Row.css";
+import axios from "../../../utils/axios";
 
-const Row = () => {
-  return (
-    <div>Row</div>
-  )
-}
+// import movieTrailer from "movie-trailer";
+// import YouTube from "react-youtube";
 
-export default Row
+const Row = ({ title, fetchUrl, isLargeRow }) => {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  const base_url = "https://image.tmdb.org/t/p/original/";
+
+  useEffect(() => {
+    (async () => {
+      try {
+        console.log(fetchUrl);
+        const request = await axios.get(`https://localhost:8000/api/${fetchUrl}`);
+        console.log(request);
+        setMovies(request.data.results);
+      } catch (error) {
+        console.log("error:", error);
+      }
+    })();
+  }, [fetchUrl]);
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie.title || movie.name || movie.original_name )
+      .then((url) => {
+        console.log(url);
+        const urlParams = new URLSearchParams(new URL(url).search);
+        console.log(urlParams);
+        console.log(urlParams.get("v"));
+        setTrailerUrl(urlParams.get("v"));
+      });
+    }
+  };
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+return (
+  <div className="row">
+    <h3>{title}</h3>
+    <div className="row_posters">
+      {movies?.map((movie, index) => (
+        <img
+          onClick={() => handleClick(movie)}
+          key={index}
+          src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+          alt={movie.name}
+          className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+        />
+      ))}
+    </div>
+
+    {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+  </div>
+);
+};
+
+export default Row;
